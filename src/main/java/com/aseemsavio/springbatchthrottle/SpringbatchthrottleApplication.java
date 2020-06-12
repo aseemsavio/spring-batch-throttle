97925c615d811afef7aa78ca13767f5dba8bd7df
aseemsavio <aseemsavio3@gmail.com>
2020-06-12 11:46:30 +0530

    Initial COmmit

---
+++ src/main/java/com/aseemsavio/springbatchthrottle/SpringbatchthrottleApplication.java
@@ -0,0 +1,13 @@
+package com.aseemsavio.springbatchthrottle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchthrottleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchthrottleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aseemsavio/springbatchthrottle/SpringbatchthrottleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aseemsavio.springbatchthrottle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchthrottleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
